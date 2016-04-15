f698fad2d64d413c0b13fca213fcc629dc03e1d3
tanyaweaver <tanya83@uw.edu>
2016-04-15 13:30:05 -0700

    mltiply done

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-function sum ( a, b ) {
-  console.log('The sum of ' + a + ' and ' + b + ' is ' + ( a + b ));
-  return( a + b );
+function sum (a, b) {
+  console.log('The sum of ' + a + ' and ' + b + ' is ' + (a + b));
+  return(a + b);
+}
+
+function multiply (a, b) {
+  console.log('The product of ' + a + ' and ' + b + ' is ' + (a * b));
+  return(a * b);
 }
